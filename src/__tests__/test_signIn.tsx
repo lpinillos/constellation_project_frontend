ba9a5f948d446ba0d20e1579ff6c0a99d3aced0a
Juan David Patiño Zambrano <juanda232405@hotmail.com>
2024-11-07 21:39:59 -0500

    test: unit test ready 🤓🐞🧪

---
+++ src/__tests__/test_signIn.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+import Signin from '../app/(auth)/signin/page';
+import LoginForm from '@/components/forms/LoginForm';
+
+jest.mock('../components/forms/LoginForm', () => jest.fn(() => <div>LoginForm Component</div>));
+
+describe('Signin Component', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render the title "Constellation"', () => {
+    render(<Signin />);
+    expect(screen.getByText('Constellation')).toBeInTheDocument();
+  });
+
+  it('should render the subtitle "Sign in."', () => {
+    render(<Signin />);
+    expect(screen.getByText('Sign in.')).toBeInTheDocument();
+  });
+
+  it('should render the description text', () => {
+    render(<Signin />);
+    expect(screen.getByText('Fill in your credentials to access your account.')).toBeInTheDocument();
+  });
+
+  it('should render the LoginForm component', () => {
+    render(<Signin />);
+    expect(screen.getByText('LoginForm Component')).toBeInTheDocument();
+  });
+
+  it('should render the Card component with correct max-width and full width', () => {
+    render(<Signin />);
+    const card = screen.getAllByRole('paragraph'); 
+    expect(card[0]).toHaveClass('text-sm text-muted-foreground');
+  });
+  
+});
